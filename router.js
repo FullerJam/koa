f3d7938ebb9305e3e258ace22c3ab788707902af
James Fuller <james.fuller@solent.ac.uk>
2023-06-29 13:42:37 +0100

    update router to use new middleware

--- router.js
+++ router.js
@@ -1,8 +1,11 @@
 const Router = require("koa-router");
 const router = new Router();
 const { getTodos, addTodo } = require("./controllers/todo.controllers");
+const { randomuserdbCache } = require("./middleware/randomuserdb.cache");
+const { getUserList } = require("./controllers/users.controllers");
 
-router.get("/todo-list", getTodos);
+router.get("/users-list", randomuserdbCache, getUserList);
+router.get("/todos-list", getTodos);
 router.post("/add-todo", addTodo);
 
 module.exports = router;
--- router/router.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router");
-const router = new Router();
-const { getTodos, postEvent } = require("./controllers/todo.controllers");
-
-router.get("/todos-list", getTodos);
-router.post("/add-todo", postEvent);
-
-module.exports = router;
